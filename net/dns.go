abd351c3bbb2fa5b018d848151069ed4914f2898
zhangheng <1049725878@qq.com>
2025-01-23 15:00:19 +0800

    优化 dns 自动权重

--- net/dns.go
+++ net/dns.go
@@ -2,6 +2,7 @@ package net
 
 import (
 	"math/rand"
+	"strings"
 	"sync"
 	"time"
 )
@@ -10,10 +11,11 @@ type dns struct {
 	host string
 	port string
 
-	ip       []string
-	delay    []time.Duration
-	sumDelay time.Duration
+	ip    []string
+	delay []time.Duration
+	//sumDelay time.Duration
 	count    []int
+	allCount int
 
 	// 权重
 	weight []int
@@ -30,17 +32,26 @@ func newDns(host string, port string, ip []string) *dns {
 		ip = make([]string, 0, 5)
 		ip = append(ip, host)
 	} else {
-		ip = append(ip, host)
+
+		tip := make([]string, 0, len(ip))
+		// 将 ipv4 加到 tip 中
+		for _, s := range ip {
+			if !strings.Contains(s, ":") {
+				tip = append(tip, s)
+			}
+		}
+		ip = append(tip, host)
 	}
 	d := &dns{
 		host: host,
 		port: port,
 
-		ip:       ip,
-		delay:    make([]time.Duration, len(ip)),
-		sumDelay: 0,
+		ip:    ip,
+		delay: make([]time.Duration, len(ip)),
+		//sumDelay: 0,
 		count:    make([]int, len(ip)),
 		weight:   make([]int, 120),
+		allCount: 0,
 
 		defaultIndex: 0,
 		lastCheck:    0,
@@ -91,7 +102,43 @@ func (ns *dns) AddOneDialed(ip string, index int, delay time.Duration) {
 
 		ns.delay[ns.weight[index]] = ns.delay[ns.weight[index]] + delay
 		ns.count[ns.weight[index]]++
-		ns.sumDelay += delay
+		//ns.sumDelay += delay
+		ns.allCount++
+
+		if (ns.allCount % (len(ns.ip) << 3)) == 0 {
+			ns.allCount = 0
+			// 计算平均延迟
+			avgs := make([]int64, len(ns.delay))
+			sumD := int64(0)
+
+			minDelay := int64(10000000)
+
+			for i := 0; i < len(ns.delay); i++ {
+				avgs[i] = int64(ns.delay[i]) / int64(ns.count[i])
+				if minDelay > avgs[i] {
+					minDelay = avgs[i]
+				}
+				sumD += avgs[i]
+				ns.delay[i] = 0
+				ns.count[i] = 1
+			}
+
+			if minDelay == 0 || minDelay > int64(time.Millisecond)*50 {
+				ns.nextDnsTime = time.Now()
+			}
+
+			wi := 0
+			// 重新计算 ns.weight, 平均延迟越低, 权重越高
+			for i := 0; i < len(avgs); i++ {
+				c := int64(len(ns.weight)) * avgs[i] / sumD
+				for ; wi < len(ns.weight) && c > 0; wi++ {
+					c--
+					ns.weight[wi] = i
+				}
+
+			}
+		}
+
 	}()
 }
 
